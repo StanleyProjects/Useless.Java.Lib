c43dd91d50301d4abedbacc41a811cc3ed523382
Stanley Wintergreen <kepocnhh@protonmail.com>
2025-08-18 19:18:46 +0300

    fix docs

--- lib/src/main/kotlin/org/kepocnhh/useless/Useless.kt
+++ lib/src/main/kotlin/org/kepocnhh/useless/Useless.kt
@@ -1,8 +1,24 @@
 package org.kepocnhh.useless
 
+/**
+ * @author [Stanley Wintergreen](https://github.com/kepocnhh)
+ * @since 0.6.0
+ */
 object Useless {
     private const val One = 1
 
+    /**
+     * This function should always return exactly 1.
+     *
+     * Usage:
+     * ```
+     * val expected = 1
+     * val actual = Useless.getOne()
+     * assertEquals(expected, actual)
+     * ```
+     * @author [Stanley Wintergreen](https://github.com/kepocnhh)
+     * @since 0.6.0
+     */
     fun getOne(): Int {
         return One
     }
@@ -11,6 +27,18 @@ object Useless {
         return getOne() + One
     }
 
+    /**
+     * This function should always return exactly 3.
+     *
+     * Usage:
+     * ```
+     * val expected = 3
+     * val actual = Useless.getThree()
+     * assertEquals(expected, actual)
+     * ```
+     * @author [Stanley Wintergreen](https://github.com/kepocnhh)
+     * @since 0.6.0
+     */
     fun getThree(): Int {
         return getTwo() + One
     }
