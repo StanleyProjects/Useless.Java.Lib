ed2479dd88ece419ff1bfd9fc0802966f1b4cb58
Stanley Wintergreen <kepocnhh@protonmail.com>
2025-08-29 19:19:53 +0300

    gradlex:0.1.0

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,12 +1,9 @@
-repositories {
-    mavenCentral()
-    maven("https://central.sonatype.com/repository/maven-snapshots")
-}
+repositories.mavenCentral()
 
 plugins {
     `kotlin-dsl`
 }
 
 dependencies {
-    implementation("com.github.kepocnhh:Gradlex:0.0.7-SNAPSHOT")
+    implementation("com.github.kepocnhh:Gradlex:0.1.0")
 }
