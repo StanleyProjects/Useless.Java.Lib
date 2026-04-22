9864dbf214cd58f653e787d9341047d2bebe5443
Stanley Wintergreen <kepocnhh@protonmail.com>
2026-04-22 20:21:32 +0300

    scripts

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -189,7 +189,7 @@ fun tasks(variant: String, version: String, maven: Maven.Artifact, gh: GitHub.Re
         archiveClassifier = "sources"
         from(sourceSets.main.get().allSource)
     }
-    tasks.register("assemble${variant}Metadata") {
+    tasks.register("assemble${variant.ufc()}Metadata") {
         doLast {
             val target = buildDir().dir("yml").file("metadata.yml")
             val file = gh.assemble(version = version, target = target)
